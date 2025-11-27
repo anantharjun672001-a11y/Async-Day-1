5d268ff394ae481f0b48057fe0dc67db17a8e2e7
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-27 19:20:59 +0530

    Callback hell updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 //callback
 
-console.log("1.start");
+/* console.log("1.start");
 
 setTimeout(()=>{
     console.log("3.timeout");
@@ -9,4 +9,45 @@ setTimeout(()=>{
 },1000)
 
 console.log("2.end");
+ */
+//callback hell
 
+/* setTimeout(()=>{    
+    console.log("1");
+    setTimeout(()=>{
+    console.log("2");
+    setTimeout(()=>{
+    console.log("3");
+    setTimeout(()=>{
+    console.log("4");
+    
+    },1000)
+    },1000)
+    },1000)
+},1000) */
+
+/* setTimeout(()=>{
+    console.log("5");
+    setTimeout(()=>{
+    console.log("4");
+    setTimeout(()=>{
+    console.log("3");
+    setTimeout(()=>{
+    console.log("2");
+    setTimeout(()=>{
+    console.log("1");
+    setTimeout(()=>{
+   
+    
+    
+},1000)
+    
+},1000)
+    
+},1000)
+    
+},1000)
+    
+},1000)
+    
+},1000) */
